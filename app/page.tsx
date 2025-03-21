0643540daf40d64d8408ac8fb616cc9c2becfd6c
Na18422 <18422b18422b@gmail.com>
2025-03-22 09:48:32 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1 style={{ color: 'blue', fontSize: '40px' }}>BUILDING MANAGEMENT</h1>
-      <h2 style={{ color: 'blue', fontSize: '40px' }}>HOME PAGE</h2>
+      <h1 style={{ fontSize: '40px' }}>BUILDING MANAGEMENT</h1>
+      <h2 style={{ fontSize: '40px' }}>HOME PAGE</h2>
       <img src="pic.png" alt="Building Image" width={500} height={300} style={{ display: 'block', margin: '0 auto' }} />
 
-      <a href="/about" target="_blank" rel="noopener noreferrer">
-          About
+      <a href="/about" target="_blank" rel="noopener noreferrer" style={{ color: 'blue', fontSize: '24px', display: 'block', marginBottom: '10px' }}>
+          click me to about
       </a>
     </div>
   );
