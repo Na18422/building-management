b470687d17a9f0ea8a3dcf8a4433aa7b3ec56ced
Na18422 <18422b18422b@gmail.com>
2025-03-22 09:40:24 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import Image from 'next/image';
 
 const Home: React.FC = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1 style={{ color: 'blue', fontSize: '40px' }}>BUILDING MANAGEMENT</h1>
+      <h1 style={{ color: 'blue', fontSize: '40px' }}>HOME PAGE</h1>
     </div>
-
-    <div style={{ textAlign: 'center' }}>
-      <Image
-        src="pic.png" 
-        alt="Building Image" 
-        width={500}           
-        height={300}         
-      />
-    </div>
+      
+      <img src="pic.png" alt="Building Image" width={500} height={300} style={{ display: 'block', margin: '0 auto' }}/>
   );
 };
 
