c5e605e75282b31e0426262ae6812344cddd5945
Na18422 <18422b18422b@gmail.com>
2025-03-21 23:53:19 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Image from 'next/image';
+
 
 const Home: React.FC = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1 style={{ color: 'blue', fontSize: '40px' }}>BUILDING MANAGEMENT</h1>
+      <Image
+        src="/building.jpg"  
+        alt="Building Image" 
+        width={500}           
+        height={300}         
+      />
     </div>
+
   );
 };
 
