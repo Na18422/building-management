c6b8989b599e723897b241f4a6f5c7c90fceeef0
Na18422 <18422b18422b@gmail.com>
2025-03-22 10:08:08 +1100

    Create route.ts

---
+++ app/api/GetBuildInfo/route.ts
@@ -0,0 +1,12 @@
+async function getBuildingAddress() {
+  const response = await fetch('https://building.com/api/building-address', {
+    method: 'GET',
+  });
+
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.address;
+  } else {
+    throw new Error("error");
+  }
+}
