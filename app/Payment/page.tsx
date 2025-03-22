94c8f18ec0b31d2e3d6d8b77c98db4b12d55c683
Na18422 <18422b18422b@gmail.com>
2025-03-22 16:00:30 +1100

    Update page.tsx

--- app/Payment/page.tsx
+++ app/Payment/page.tsx
@@ -1,1 +1,19 @@
+import React from 'react';
 
+const Payment: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1 style={{ fontSize: '80px', backgroundColor: '#ADD8E6', color: 'white'}}>BUILDING MANAGEMENT</h1>
+      <br /><br />
+      <h2 style={{ fontSize: '40px' }}>Payment</h2>
+      <br /><br />
+
+      <br /><br />
+      <a href="https://building-management-l77j.vercel.app/" rel="noopener noreferrer" style={{ color: 'blue', fontSize: '24px', display: 'block', marginBottom: '10px' }}>
+          Back To Home
+      </a>
+    </div>
+  );
+};
+
+export default About;
