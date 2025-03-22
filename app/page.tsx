8ae0e9c73952c18bb342d697e3da3470a30ade89
Na18422 <18422b18422b@gmail.com>
2025-03-22 13:01:53 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ const Home: React.FC = () => {
       <h1 style={{ fontSize: '80px', backgroundColor: '#ADD8E6', color: 'white'}}>BUILDING MANAGEMENT</h1>
       <br /><br />
       <h2 style={{ fontSize: '40px' }}>HOME PAGE</h2>
+      <br /><br />
       <img src="pic.png" alt="Building Image" width={500} height={300} style={{ display: 'block', margin: '0 auto' }} />
       <br /><br />
       <a href="/about"  rel="noopener noreferrer" style={{ color: 'blue', fontSize: '24px', display: 'block', marginBottom: '10px' }}>
