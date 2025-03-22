108f3366733cc9c4fa34a907aa72add61d385abd
Na18422 <18422b18422b@gmail.com>
2025-03-22 12:54:08 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div style={{ textAlign: 'center'}}>
-      <h1 style={{ fontSize: '80px' }}>BUILDING MANAGEMENT</h1>
+      <h1 style={{ fontSize: '80px', backgroundColor: 'blue', color: 'white'}}>BUILDING MANAGEMENT</h1>
       <h2 style={{ fontSize: '40px' }}>HOME PAGE</h2>
       <img src="pic.png" alt="Building Image" width={500} height={300} style={{ display: 'block', margin: '0 auto' }} />
       <br /><br />
