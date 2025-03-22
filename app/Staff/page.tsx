0bed12e8f0a97287908db6d89f93727a63b19122
Na18422 <18422b18422b@gmail.com>
2025-03-22 13:12:50 +1100

    Update page.tsx

--- app/Staff/page.tsx
+++ app/Staff/page.tsx
@@ -1,1 +1,14 @@
+import React from 'react';
 
+const Staff: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center'}}>
+      <h1 style={{ fontSize: '80px', backgroundColor: '#ADD8E6', color: 'white'}}>BUILDING MANAGEMENT</h1>
+      <br /><br />
+      <h2 style={{ fontSize: '40px' }}>Staff</h2>
+      <br /><br />
+      
+  );
+};
+
+export default Staff;
